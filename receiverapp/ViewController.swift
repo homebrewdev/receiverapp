3d18b860ff63a730d68ff125a811a46b23732ffe
Edeviatov <edeviatov@htc-cs.ru>
2019-08-14 16:48:22 +0400

    Initial Commit

---
+++ receiverapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  receiverapp
+//
+//  Created by Egor Devyatov on 14/08/2019.
+//  Copyright © 2019 Egor Devyatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
